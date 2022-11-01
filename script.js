0ded2aa66467a8375b83d6203f325fb57d74a731
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-01 15:02:12 +0500

    Add UI class to Handle UI tasks

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ class Book {
     this.author = author;
   }
 }
+
+// UI Class: Handle UI Tasks
+
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: 'How to become a Software developer',
+        author: 'Nabeel Akbar',
+      },
+      {
+        title: 'How to be successful as Process Technician',
+        author: 'Sane Myburg',
+      },
+      {
+        title: 'Love at first sight',
+        author: 'Precious Nhlapho',
+      },
+    ];
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
